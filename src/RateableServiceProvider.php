6811c2e7b3c5664e2443c1b066f9423f68789ed5
Kuddus <ma.kuddus37@gmail.com>
2020-12-12 10:57:09 +0600

    rate and reating

---
+++ src/RateableServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace mak\Rateable;
+
+use Illuminate\Support\ServiceProvider;
+
+class RateableServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            if (!class_exists('CreateRatingsTable')) {
+                $this->publishes([
+                    __DIR__ . '/../database/migrations/create_ratings_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_ratings_table.php'),
+                ], 'migrations');
+            }
+        }
+    }
+
+    public function register()
+    {
+    }
+}
